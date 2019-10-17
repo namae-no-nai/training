01d75f656b6d799912b1e2020839fa09f521f0be
namae-no-nai <guilhermebluiz@hotmail.com>
2019-10-16 22:51:00 -0300

    prime numbers

---
+++ next_prime.c
@@ -0,0 +1,36 @@
+int	ft_is_prime(int nb)
+{
+	int i;
+
+	if (nb <= 1)
+		return (0);
+	if (nb == 2 || nb == 3)
+		return (1);
+	if (nb % 2 == 0 || nb % 3 == 0)
+		return (0);
+	i = 3;
+	while (i * i <= nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (ft_is_prime(nb))
+		return (nb);
+	if (nb <= 2)
+		return (2);
+	if (nb % 2 == 0)
+		nb++;
+	while (nb < 2147483647)
+	{
+		if (ft_is_prime(nb))
+			return (nb);
+		nb += 2;
+	}
+	return (-1);
+}
